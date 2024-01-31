68315dd930fdbd307fd5371a7dd47ce18e7620f0
AndrewH1188 <andrewh1188@gmail.com>
2024-01-31 22:40:43 +0000

    Added more hero images, added missing footer to heroes pages and the code I used in my pervious WHR project for the show more/less button - this has to be worked on to function correctly.

---
+++ assets/js/heroes.js
@@ -0,0 +1,24 @@
+//////DO NOT DELETE BELOW THIS LINE
+
+// JS from demos2
+$(document).ready(function () {
+    $("button").click(function () {
+      $("article").toggle();
+    });
+  });
+  
+  
+  // The code below taken from Stack Overflow from same issue from someone else's problem.
+  
+  //this will execute on page load(to be more specific when document ready event occurs)
+  if ($('.ty-compact-list').length > 3) {
+    $('.ty-compact-list:gt(2)').hide();
+    $('.btn-heroes-pages').show();
+  }
+  
+  $('.btn-heroes-pages').on('click', function () {
+    //toggle elements with class .ty-compact-list that their index is bigger than 2
+    $('.ty-compact-list:gt(2)').toggle();
+    //change text of show more element just for demonstration purposes to this demo
+    $(this).text() === 'Show less' ? $(this).text('Show more') : $(this).text('Show less');
+  });

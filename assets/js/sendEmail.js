64cc6a8ff10923451f2a6a874d20f5fdc469fffe
AndrewH1188 <andrewh1188@gmail.com>
2024-02-05 22:26:09 +0000

    Contact form functionality added.

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,21 @@
+// Code from Rosie's Resume and slightly amended
+let contactForm = document.getElementById("contactForm")
+contactForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+    sendMail()
+})
+
+function sendMail() {
+    emailjs.send("service_rzruu2w","template_u1o72eq", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "website_enquiry": contactForm.enquirytype.value,
+        })
+        .then(function () {
+            console.log('SUCCESS!');
+            alert('Thank you for contacting us, we have recieved your enquiry and will reply to you soon')
+        }, function (error) {
+            console.log('FAILED...', error);
+            alert('We had a problem with sending your message, please try again.')
+        });
+}
